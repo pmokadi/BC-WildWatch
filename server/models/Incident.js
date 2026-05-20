f6a0959bf4830693c496cfbd191ac209b34fd556
Paballo Mokadi <pabi1mokadi@gmail.com>
2026-05-20 23:50:57 +0200

    incident collection created

---
+++ server/models/Incident.js
@@ -0,0 +1,47 @@
+//Incident - Defines the structure of an emergency incident report when a student has direct encounter with a dangerous animal/insect
+
+const mongoose = require("mongoose");
+
+const incidentSchema = new mongoose.Schema({
+    // will link the incident to student who reported it
+    reporter: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    urgency: {
+        type: String,
+        required: true,
+        enum: ["Low", "Medium", "High"]
+    },
+
+    situationDescription: {
+        type: string,
+        required: true,
+        trim: true
+    },
+
+    //path to uploaded image/video attached by user
+    mediaUrl: {
+        type: string,
+        default: null
+    },
+
+    status: {
+        type: string,
+        enum: ["Open", "In Progress", "Resolved"],
+        default: "Open"
+    },
+
+    //admin can add notes when reviewing the incident
+    adminNotes: {
+        type: string,
+        default: ""
+    }
+},
+{
+    timestamps: true // Automatically add createdAt and updatedAt fields
+});
+
+module.exports = mongoose.model("Incident", incidentSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
 
     role: {
         type: String,
-        enum: ["student", "admin"],
+        enum: ["student", "admin", "security"],
         default: "student"
     },
 
