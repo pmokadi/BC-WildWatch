e10606713d0a3b0189fa06111f7bbd8381f48afa
Paballo Mokadi <pabi1mokadi@gmail.com>
2026-05-20 23:15:57 +0200

    user collection created

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+// User Model - defines the structure of user data in the database
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    // Password will be hashed before saving and never stored as text
+    password:{
+        type: String,
+        required: true,
+        minlength: 8,
+        select: false, // Exclude password from query results by default
+    },
+
+    role: {
+        type: String,
+        enum: ["student", "admin"],
+        default: "student"
+    },
+
+    campus: {
+        type: String,
+        default: "Main Campus - Pretoria"
+    }
+},
+{
+    timestamps: true // Automatically add createdAt and updatedAt fields
+});
+
+//Hashing the password before saving to database
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password"))
+        return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+// Method checking if entered password matches stored hash
+userSchema.methods.comparePassword = async function (enteredPassword)
+{
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
